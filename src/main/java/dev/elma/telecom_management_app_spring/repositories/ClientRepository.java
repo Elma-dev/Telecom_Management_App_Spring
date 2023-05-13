8df91886a020f1dba902384331c0b4f795105f67
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-05-13 19:05:07 +0100

    commit

--- src/main/java/dev/elma/telecom_management_app_spring/repositories/ClientRepository.java
+++ src/main/java/dev/elma/telecom_management_app_spring/repositories/ClientRepository.java
@@ -1,7 +1,10 @@
 package dev.elma.telecom_management_app_spring.repositories;
 
 import dev.elma.telecom_management_app_spring.entities.Client;
+import org.hibernate.sql.Update;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -11,4 +14,5 @@ import java.util.Optional;
 public interface ClientRepository extends JpaRepository<Client,Long> {
 
     Optional<Client> findByName(String name);
+
 }
--- src/main/java/dev/elma/telecom_management_app_spring/web/RestWebServices.java
+++ src/main/java/dev/elma/telecom_management_app_spring/web/RestWebServices.java
@@ -1,34 +1,52 @@
 package dev.elma.telecom_management_app_spring.web;
 
 import dev.elma.telecom_management_app_spring.entities.Client;
+import dev.elma.telecom_management_app_spring.entities.Subscribe;
 import dev.elma.telecom_management_app_spring.repositories.ClientRepository;
+import dev.elma.telecom_management_app_spring.repositories.SubscribeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/clients")
+//@RequestMapping("/clients")
 @AllArgsConstructor
 public class RestWebServices {
     private ClientRepository clientRepository;
+    private SubscribeRepository subscribeRepository;
 
-    @GetMapping
+    @GetMapping("/clients")
     public List<Client> getAllClients(){
         return this.clientRepository.findAll();
     }
-    @GetMapping("/{id}")
+    @GetMapping("/clients/{id}")
     public Client findClientById(@PathVariable(name = "id") long id){
         return this.clientRepository.findById(id).get();
     }
 
-    @PostMapping("/add")
+    @PostMapping("/clients/add")
     public Client addClient(@RequestBody Client client){
         return this.clientRepository.save(client);
     }
 
-    @GetMapping("/delete/{id}")
+    @DeleteMapping("/clients/delete/{id}")
     public void deleteClient(@PathVariable(name = "id") long id){
         this.clientRepository.deleteById(id);
     }
+
+    @PutMapping("/clients/update/{id}")
+    public Client updateClient(@PathVariable long id,@RequestBody Client client){
+        Client client1 = this.clientRepository.findById(id).get();
+        client1.setName(client.getName());
+        client1.setEmail(client1.getEmail());
+        client1.setUsername(client1.getUsername());
+        return this.clientRepository.save(client1);
+    }
+
+    @GetMapping("/subscribers")
+    public List<Subscribe> getAllSubscribers(){
+        return subscribeRepository.findAll();
+    }
+
 }
