7f90c9cb43b53bbb2573dbb5a2e940354bd7aa6c
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-05-13 18:16:11 +0100

    commit

--- src/main/java/dev/elma/telecom_management_app_spring/repositories/ClientRepository.java
+++ src/main/java/dev/elma/telecom_management_app_spring/repositories/ClientRepository.java
@@ -2,10 +2,12 @@ package dev.elma.telecom_management_app_spring.repositories;
 
 import dev.elma.telecom_management_app_spring.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 import java.util.Optional;
 
+//@RepositoryRestResource
 public interface ClientRepository extends JpaRepository<Client,Long> {
 
     Optional<Client> findByName(String name);
--- src/main/java/dev/elma/telecom_management_app_spring/repositories/SubscribeRepository.java
+++ src/main/java/dev/elma/telecom_management_app_spring/repositories/SubscribeRepository.java
@@ -2,7 +2,9 @@ package dev.elma.telecom_management_app_spring.repositories;
 
 import dev.elma.telecom_management_app_spring.entities.Subscribe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource
 public interface SubscribeRepository extends JpaRepository<Subscribe,Long> {
 
 }
