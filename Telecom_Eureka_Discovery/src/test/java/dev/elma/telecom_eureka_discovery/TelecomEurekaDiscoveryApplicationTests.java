9053982b9645fda90e5e090eaa5202d27e69ce98
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-05-14 17:53:16 +0100

    commit

---
+++ Telecom_Eureka_Discovery/src/test/java/dev/elma/telecom_eureka_discovery/TelecomEurekaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.telecom_eureka_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomEurekaDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
